e3067521b6baa93ffe83f8fc8ec6bb254d49d38b
Djamal54321 <Djamalik05@mail.ru>
2023-01-31 11:17:07 +0600

    through map

--- src/components/PageTwo/PageTwo.jsx
+++ src/components/PageTwo/PageTwo.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import './PageTwo.css';
+import Block from './Block.jsx';
+
+let blockData = [
+  { element: '1' },
+  { element: '2' },
+  { element: '3' },
+  { element: '4' },
+  { element: '5' },
+  { element: '6' },
+  { element: '7' },
+  { element: '8' },
+];
+
+let block = blockData.map((el) => <Block block={el} />);
 
 function Page() {
   return (
     <div className="page">
+      <button className="two"></button>
       <div className="header">
         <input className="input" type="text"></input>
       </div>
-      <div className="blocks">
-        <div className="block"></div>
-        <div className="block"></div>
-        <div className="block"></div>
-        <div className="block"></div>
-        <div className="block"></div>
-        <div className="block"></div>
-        <div className="block"></div>
-        <div className="block"></div>
-      </div>
+      <div className="blocks">{block}</div>
     </div>
   );
 }
